bc78bb9510161b70da84caca972ec5e5a966bb41
Donald Pinckney <donald_pinckney@icloud.com>
2022-06-27 15:50:56 -0400

    Script to make tests

---
+++ relational_db_builder/grab_seq_testcase.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+seq=$1
+
+psql -p 5431 npm_data -c "SELECT raw_json FROM change_log where seq = $seq;" -t -A -o $SCRIPT_DIR/resources/test_changes/seq_$seq.json.tmp
+python -m json.tool $SCRIPT_DIR/resources/test_changes/seq_$seq.json.tmp > $SCRIPT_DIR/resources/test_changes/seq_$seq.json
+rm $SCRIPT_DIR/resources/test_changes/seq_$seq.json.tmp
