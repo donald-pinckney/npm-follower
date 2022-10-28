f1454a82906a35cba735e40e7ded3238be28a15c
Donald Pinckney <donald_pinckney@icloud.com>
2022-10-28 18:06:35 -0400

    small perf diff, leave note for later

--- postgres_db/src/packument.rs
+++ postgres_db/src/packument.rs
@@ -30,14 +30,15 @@ pub enum PackageOnlyPackument {
 
 impl PackageOnlyPackument {
     pub fn serialize_and_hash(&self) -> (Value, String, usize) {
-        let v = serde_json::to_value(self).unwrap();
-        let s = serde_json::to_vec(&v).unwrap();
+        // let v = serde_json::to_value(self).unwrap();
+        let s = serde_json::to_vec(self).unwrap();
         let n_bytes = s.len();
         let mut hasher = Sha256::new();
         hasher.update(s);
         let result = hasher.finalize();
 
-        (v, format!("{:x}", result), n_bytes)
+        // TODO[perf]: Return a serialized JSON value here, and pass it through to DB inserts later so we can save on serailization cost.
+        (Value::Null, format!("{:x}", result), n_bytes)
     }
 
     /// Returns `true` if the package only packument is [`Normal`].
@@ -65,14 +66,15 @@ pub struct VersionOnlyPackument {
 
 impl VersionOnlyPackument {
     pub fn serialize_and_hash(&self) -> (Value, String, usize) {
-        let v = serde_json::to_value(self).unwrap();
-        let s = serde_json::to_vec(&v).unwrap();
+        // let v = serde_json::to_value(self).unwrap();
+        let s = serde_json::to_vec(self).unwrap();
         let n_bytes = s.len();
         let mut hasher = Sha256::new();
         hasher.update(s);
         let result = hasher.finalize();
 
-        (v, format!("{:x}", result), n_bytes)
+        // TODO[perf]: Return a serialized JSON value here, and pass it through to DB inserts later so we can save on serailization cost.
+        (Value::Null, format!("{:x}", result), n_bytes)
     }
 }
 
