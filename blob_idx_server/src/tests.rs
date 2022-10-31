491b0a331f5c53aa537eda044d24a2e968b2f1ae
elleven11 <federico.cassano@federico.codes>
2022-10-31 14:09:16 -0400

    more tests!

--- blob_idx_server/src/tests.rs
+++ blob_idx_server/src/tests.rs
@@ -1,39 +1,43 @@
+use std::{sync::Arc, time::Duration};
+
 use lazy_static::lazy_static;
+use tokio::sync::Mutex;
 use tokio::task::JoinHandle;
 
 use crate::{
     blob::{BlobOffset, BlobStorageConfig, BlobStorageSlice},
-    http::{BlobEntry, CreateAndLockRequest, CreateUnlockRequest, LookupRequest, HTTP},
+    http::{
+        BlobEntry, CreateAndLockRequest, CreateUnlockRequest, KeepAliveLockRequest, LookupRequest,
+        HTTP,
+    },
 };
 
 lazy_static! {
-    static ref GLOBAL_LOCK: tokio::sync::Mutex<()> = tokio::sync::Mutex::new(());
+    static ref GLOBAL_LOCK: Mutex<()> = Mutex::new(());
+    // NOTE: we use db 5 for testing. make sure you don't have anything important there.
+    static ref REDIS_TEST_DB: &'static str = "redis://127.0.0.1/5";
 }
 
 macro_rules! blob_test {
     ($body:block, $cfg:expr) => {
         let _lock = GLOBAL_LOCK.lock().await;
         redis_cleanup();
-        let server = run_test_server($cfg).await; // we have 1 file max
-
-        // wait for the server to start
-        while let Err(_) = reqwest::get("http://127.0.0.1:1337/").await {
-            tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-        }
+        let server = run_test_server($cfg).await;
 
         $body;
+
         server.shutdown().await;
         drop(_lock);
     };
     // default config
     ($body:block) => {
-        blob_test!($body, make_config(1, 5));
+        blob_test!($body, make_config(1, 5)); // we have 1 file max
     };
 }
 
 fn make_config(max_files: u32, lock_timeout: u64) -> BlobStorageConfig {
     BlobStorageConfig {
-        redis_url: "redis://127.0.0.1/5".to_string(), // NOTE: we use db 5 for testing
+        redis_url: REDIS_TEST_DB.to_string(),
         max_files,
         lock_timeout,
     }
@@ -44,7 +48,7 @@ fn simple_config() -> BlobStorageConfig {
 }
 
 fn redis_cleanup() {
-    let client = redis::Client::open("redis://127.0.0.1/5").unwrap();
+    let client = redis::Client::open(REDIS_TEST_DB.to_string()).unwrap();
     let mut con = client.get_connection().unwrap();
     redis::cmd("FLUSHDB").query::<()>(&mut con).unwrap();
 }
@@ -72,6 +76,11 @@ async fn run_test_server(cfg: BlobStorageConfig) -> TestServer {
         .await
         .unwrap()
     });
+
+    // wait for the server to start
+    while (reqwest::get("http://127.0.0.1:1337/").await).is_err() {
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    }
 
     TestServer {
         shutdown_signal: tx,
@@ -102,6 +111,20 @@ async fn send_create_unlock_request(
 ) -> (reqwest::StatusCode, String) {
     let resp = client
         .post("http://127.0.0.1:1337/create_unlock")
+        .body(serde_json::to_string(&req).unwrap())
+        .send()
+        .await
+        .unwrap();
+
+    (resp.status(), resp.text().await.unwrap())
+}
+
+async fn send_keepalive_request(
+    client: &reqwest::Client,
+    req: KeepAliveLockRequest,
+) -> (reqwest::StatusCode, String) {
+    let resp = client
+        .post("http://127.0.0.1:1337/keep_alive_lock")
         .body(serde_json::to_string(&req).unwrap())
         .send()
         .await
@@ -442,4 +465,361 @@ async fn test_lock_wait() {
         assert!(!o3.needs_creation);
         assert_eq!(o3.byte_offset, 18);
     });
+}
+
+#[tokio::test]
+async fn test_cleaner_basic() {
+    let client = reqwest::Client::new();
+    let cfg = make_config(1, 2); // two seconds to clean
+    blob_test!(
+        {
+            // lock n1
+            let resp1 = send_create_and_lock_request(
+                &client,
+                CreateAndLockRequest {
+                    entries: vec![BlobEntry::new("k1".to_string(), 1)],
+                    node_id: "n1".to_string(),
+                },
+            )
+            .await
+            .unwrap();
+
+            // wait two seconds
+            tokio::time::sleep(Duration::from_secs(2)).await;
+
+            // lock another one, should be same file.
+            let resp2 = send_create_and_lock_request(
+                &client,
+                CreateAndLockRequest {
+                    entries: vec![BlobEntry::new("k2".to_string(), 1)],
+                    node_id: "n2".to_string(),
+                },
+            )
+            .await
+            .unwrap();
+
+            // check that it's the same file
+            assert_eq!(resp1.file_id, resp2.file_id);
+
+            // unlock n2
+
+            let resp = send_create_unlock_request(
+                &client,
+                CreateUnlockRequest {
+                    file_id: resp2.file_id,
+                    node_id: "n2".to_string(),
+                },
+            )
+            .await;
+
+            println!("resp: {}", resp.1);
+            assert_eq!(resp.0, 200);
+
+            // ok now let's test for keepalive (at least 3)
+
+            // lock n1
+
+            let resp = send_create_and_lock_request(
+                &client,
+                CreateAndLockRequest {
+                    entries: vec![BlobEntry::new("k3".to_string(), 1)],
+                    node_id: "n1".to_string(),
+                },
+            )
+            .await
+            .unwrap();
+
+            // wait some time
+            tokio::time::sleep(Duration::from_millis(250)).await;
+
+            // keep alive
+            let keep1 = send_keepalive_request(
+                &client,
+                KeepAliveLockRequest {
+                    file_id: resp.file_id,
+                },
+            )
+            .await;
+            assert_eq!(keep1.0, 200);
+
+            // wait some time
+            tokio::time::sleep(Duration::from_millis(250)).await;
+
+            // keep alive
+
+            let keep2 = send_keepalive_request(
+                &client,
+                KeepAliveLockRequest {
+                    file_id: resp.file_id,
+                },
+            )
+            .await;
+            assert_eq!(keep2.0, 200);
+
+            // wait some time
+            tokio::time::sleep(Duration::from_millis(250)).await;
+
+            // keep alive
+
+            let keep3 = send_keepalive_request(
+                &client,
+                KeepAliveLockRequest {
+                    file_id: resp.file_id,
+                },
+            )
+            .await;
+            assert_eq!(keep3.0, 200);
+
+            // now check that we can unlock manually
+
+            let u = send_create_unlock_request(
+                &client,
+                CreateUnlockRequest {
+                    file_id: resp.file_id,
+                    node_id: "n1".to_string(),
+                },
+            )
+            .await;
+
+            println!("resp: {}", u.1);
+            assert_eq!(u.0, 200);
+
+            // now lets keepalive, but not unlock
+
+            let resp = send_create_and_lock_request(
+                &client,
+                CreateAndLockRequest {
+                    entries: vec![BlobEntry::new("k4".to_string(), 1)],
+                    node_id: "n1".to_string(),
+                },
+            )
+            .await
+            .unwrap();
+
+            // wait some time
+            tokio::time::sleep(Duration::from_millis(250)).await;
+
+            // keep alive
+
+            let keep1 = send_keepalive_request(
+                &client,
+                KeepAliveLockRequest {
+                    file_id: resp.file_id,
+                },
+            )
+            .await;
+
+            println!("keep1: {}", keep1.1);
+            assert_eq!(keep1.0, 200);
+
+            // wait two seconds
+
+            tokio::time::sleep(Duration::from_secs(2)).await;
+
+            // check that we can't unlock
+
+            let u = send_create_unlock_request(
+                &client,
+                CreateUnlockRequest {
+                    file_id: resp.file_id,
+                    node_id: "n1".to_string(),
+                },
+            )
+            .await;
+
+            println!("resp: {}", u.1);
+            assert_eq!(u.0, 400);
+
+            // check k4 not written
+            let resp = send_lookup_request(
+                &client,
+                LookupRequest {
+                    key: "k4".to_string(),
+                },
+            )
+            .await;
+
+            assert!(resp.is_err());
+            assert!(resp.unwrap_err().contains("Blob is not written"));
+        },
+        cfg
+    );
+}
+
+/// This tests if there exists a race condition between cleaning and keepalive.
+/// Runs tasks in parallel, one that keeps a lock alive, and another that cleans
+#[tokio::test]
+async fn test_cleaner_keepalive_race() {
+    let client = reqwest::Client::new();
+    let cfg = make_config(1, 1); // zero second to clean
+    let epochs = 20;
+    blob_test!(
+        {
+            let mut handles = vec![];
+            let order_ids: Arc<Mutex<Vec<u32>>> = Arc::new(Mutex::new(vec![]));
+            for i in 0..epochs {
+                let client = client.clone();
+                let order_ids = order_ids.clone();
+                handles.push(tokio::task::spawn(async move {
+                    // lock
+                    let resp = send_create_and_lock_request(
+                        &client,
+                        CreateAndLockRequest {
+                            entries: vec![BlobEntry::new(format!("k{}", i), 1)],
+                            node_id: "n1".to_string(),
+                        },
+                    )
+                    .await
+                    .unwrap();
+
+                    {
+                        order_ids.lock().await.push(i);
+                    }
+
+                    // send keepalive, unlock
+
+                    let keep = send_keepalive_request(
+                        &client,
+                        KeepAliveLockRequest {
+                            file_id: resp.file_id,
+                        },
+                    )
+                    .await;
+
+                    println!("keep: {}", keep.1);
+                    assert_eq!(keep.0, 200);
+
+                    let u = send_create_unlock_request(
+                        &client,
+                        CreateUnlockRequest {
+                            file_id: resp.file_id,
+                            node_id: "n1".to_string(),
+                        },
+                    )
+                    .await;
+
+                    println!("resp: {}", u.1);
+                    assert_eq!(u.0, 200);
+                }));
+            }
+
+            for h in handles {
+                h.await.unwrap();
+            }
+
+            // now check that all the files are written in order
+            let order_ids = order_ids.lock().await;
+            let mut prev = -1;
+            for o in order_ids.iter() {
+                if (*o as i64) < prev {
+                    panic!("not in order");
+                }
+                prev = *o as i64;
+            }
+        },
+        cfg
+    );
+}
+
+#[tokio::test]
+async fn more_than_one_file() {
+    let client = reqwest::Client::new();
+    let cfg = make_config(3, 20); // 3 files
+
+    blob_test!(
+        {
+            // send h1
+            let client_cl = client.clone();
+            let h1 = tokio::task::spawn(async move {
+                send_create_and_lock_request(
+                    &client_cl,
+                    CreateAndLockRequest {
+                        entries: vec![BlobEntry::new("k1".to_string(), 1)],
+                        node_id: "n1".to_string(),
+                    },
+                )
+                .await
+            });
+            // send h2
+            let client_cl = client.clone();
+            let h2 = tokio::task::spawn(async move {
+                send_create_and_lock_request(
+                    &client_cl,
+                    CreateAndLockRequest {
+                        entries: vec![BlobEntry::new("k2".to_string(), 1)],
+                        node_id: "n1".to_string(),
+                    },
+                )
+                .await
+            });
+            // send h3
+            let client_cl = client.clone();
+            let h3 = tokio::task::spawn(async move {
+                send_create_and_lock_request(
+                    &client_cl,
+                    CreateAndLockRequest {
+                        entries: vec![BlobEntry::new("k3".to_string(), 1)],
+                        node_id: "n1".to_string(),
+                    },
+                )
+                .await
+            });
+
+            // only unlock one of them
+            let resp = h2.await.unwrap().unwrap();
+            let u = send_create_unlock_request(
+                &client,
+                CreateUnlockRequest {
+                    file_id: resp.file_id,
+                    node_id: "n1".to_string(),
+                },
+            )
+            .await;
+
+            println!("resp: {}", u.1);
+            assert_eq!(u.0, 200);
+
+            // try to lock another one, should be the same as resp.file_id
+
+            let resp = send_create_and_lock_request(
+                &client,
+                CreateAndLockRequest {
+                    entries: vec![BlobEntry::new("k4".to_string(), 1)],
+                    node_id: "n1".to_string(),
+                },
+            )
+            .await
+            .unwrap();
+
+            assert_eq!(resp.file_id, resp.file_id);
+
+            // unlock all
+            let resp = h1.await.unwrap().unwrap();
+            let u = send_create_unlock_request(
+                &client,
+                CreateUnlockRequest {
+                    file_id: resp.file_id,
+                    node_id: "n1".to_string(),
+                },
+            )
+            .await;
+
+            println!("resp: {}", u.1);
+            assert_eq!(u.0, 200);
+
+            let resp = h3.await.unwrap().unwrap();
+            let u = send_create_unlock_request(
+                &client,
+                CreateUnlockRequest {
+                    file_id: resp.file_id,
+                    node_id: "n1".to_string(),
+                },
+            )
+            .await;
+
+            println!("resp: {}", u.1);
+            assert_eq!(u.0, 200);
+        },
+        cfg
+    );
 }
