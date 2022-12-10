a405a026618529edb5a40ac44ea5d9f157287291
Donald Pinckney <donald_pinckney@icloud.com>
2022-12-09 19:39:29 -0500

    bigger dep cache

--- relational_db_builder/src/relational_db_accessor.rs
+++ relational_db_builder/src/relational_db_accessor.rs
@@ -43,9 +43,7 @@ impl RelationalDbAccessor {
             package_id_cache: LruCache::new(NonZeroUsize::new(0x100000).unwrap()), // about 1 million entries
             package_data_cache: LruCache::new(NonZeroUsize::new(1_073_741_824).unwrap()), // 1GB max memory usage
             version_id_cache: LruCache::new(NonZeroUsize::new(0x100000).unwrap()), // about 1 million entries
-            dependency_states_cache: LruCache::new(NonZeroUsize::new(0x100000).unwrap()),
-            // dependencies_to_flush: HashSet::new(),
-            // dependencies_with_missing_dst: HashMap::new(),
+            dependency_states_cache: LruCache::new(NonZeroUsize::new(0x1000000).unwrap()), // about 16 million entries
         }
     }
 }
