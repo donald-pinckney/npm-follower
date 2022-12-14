3caf602449f45d79846629480fb94768273a6dc2
Donald Pinckney <donald_pinckney@icloud.com>
2022-12-14 11:32:00 -0500

    fix internal state

--- postgres_db/src/internal_state.rs
+++ postgres_db/src/internal_state.rs
@@ -4,47 +4,83 @@ use super::schema;
 use diesel::prelude::*;
 
 pub fn query_diff_log_processed_seq<R: QueryRunner>(conn: &mut R) -> Option<i64> {
-    query_key_value_state("diff_log_processed_seq", conn)
+    query_key_value_int_state("diff_log_processed_seq", conn)
 }
 
 pub fn set_diff_log_processed_seq<R: QueryRunner>(seq: i64, conn: &mut R) {
-    set_key_value_state("diff_log_processed_seq", seq, conn);
+    set_key_value_int_state("diff_log_processed_seq", seq, conn);
 }
 
 pub fn query_relational_processed_seq<R: QueryRunner>(conn: &mut R) -> Option<i64> {
-    query_key_value_state("relational_processed_seq", conn)
+    query_key_value_int_state("relational_processed_seq", conn)
 }
 
 pub fn set_relational_processed_seq<R: QueryRunner>(seq: i64, conn: &mut R) {
-    set_key_value_state("relational_processed_seq", seq, conn);
+    set_key_value_int_state("relational_processed_seq", seq, conn);
 }
 
 pub fn query_queued_downloads_seq<R: QueryRunner>(conn: &mut R) -> Option<i64> {
-    query_key_value_state("queued_downloads_seq", conn)
+    query_key_value_int_state("queued_downloads_seq", conn)
 }
 
 pub fn set_queued_downloads_seq<R: QueryRunner>(seq: i64, conn: &mut R) {
-    set_key_value_state("queued_downloads_seq", seq, conn);
+    set_key_value_int_state("queued_downloads_seq", seq, conn);
 }
 
-fn query_key_value_state<R: QueryRunner>(the_key: &str, conn: &mut R) -> Option<i64> {
+pub fn query_gha_pointer<R: QueryRunner>(conn: &mut R) -> Option<String> {
+    query_key_value_string_state("gha_pointer", conn)
+}
+
+pub fn set_gha_pointer<R: QueryRunner>(pointer: String, conn: &mut R) {
+    set_key_value_string_state("gha_pointer", pointer, conn);
+}
+
+fn query_key_value_int_state<R: QueryRunner>(the_key: &str, conn: &mut R) -> Option<i64> {
     use schema::internal_state::dsl::*;
 
-    conn.first(internal_state.filter(key.eq(the_key)).select(value))
+    let nested: Option<Option<i64>> = conn
+        .first(internal_state.filter(key.eq(the_key)).select(int_value))
         .optional()
-        .expect("Error accessing table: internal_state")
+        .expect("Error accessing table: internal_state");
+
+    nested.flatten()
 }
 
-fn set_key_value_state<R: QueryRunner>(the_key: &str, the_value: i64, conn: &mut R) {
+fn set_key_value_int_state<R: QueryRunner>(the_key: &str, the_value: i64, conn: &mut R) {
     use schema::internal_state::dsl::*;
 
-    let new_pair = (key.eq(the_key), value.eq(the_value));
+    let new_pair = (key.eq(the_key), int_value.eq(the_value));
     conn.execute(
         diesel::insert_into(internal_state)
             .values(&new_pair)
             .on_conflict(key)
             .do_update()
             .set(new_pair),
+    )
+    .unwrap_or_else(|_| panic!("Failed to set key/value pair: {:?}", new_pair));
+}
+
+fn query_key_value_string_state<R: QueryRunner>(the_key: &str, conn: &mut R) -> Option<String> {
+    use schema::internal_state::dsl::*;
+
+    let nested: Option<Option<String>> = conn
+        .first(internal_state.filter(key.eq(the_key)).select(string_value))
+        .optional()
+        .expect("Error accessing table: internal_state");
+
+    nested.flatten()
+}
+
+fn set_key_value_string_state<R: QueryRunner>(the_key: &str, the_value: String, conn: &mut R) {
+    use schema::internal_state::dsl::*;
+
+    let new_pair = (key.eq(the_key), string_value.eq(the_value));
+    conn.execute(
+        diesel::insert_into(internal_state)
+            .values(&new_pair)
+            .on_conflict(key)
+            .do_update()
+            .set(new_pair.clone()),
     )
     .unwrap_or_else(|_| panic!("Failed to set key/value pair: {:?}", new_pair));
 }
