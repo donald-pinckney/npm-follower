782ea3bd1ed0212f073653e38779d22a4ddacd3d
Donald Pinckney <donald_pinckney@icloud.com>
2022-12-25 17:48:52 -0500

    x

--- analysis/clean/build_updates.sql
+++ analysis/clean/build_updates.sql
@@ -1,2 +1,4 @@
 DROP TABLE IF EXISTS analysis.all_updates;
 DROP TABLE IF EXISTS analysis.all_overlaps;
+DROP FUNCTION IF EXISTS analysis.determine_update_type;
+DROP TYPE IF EXISTS analysis.update_type;
--- analysis/scripts/build_updates.sql
+++ analysis/scripts/build_updates.sql
@@ -1,12 +1,28 @@
+CREATE TYPE analysis.update_type AS ENUM ('zero_to_something', 'bug', 'minor', 'major');
+
+CREATE OR REPLACE FUNCTION analysis.determine_update_type(semver, semver) RETURNS analysis.update_type AS $$
+-- $1 = from
+-- $2 = to
+SELECT CASE
+        WHEN ($1) >= ($2) THEN NULL
+        WHEN ($1).prerelease IS NOT NULL OR ($1).build IS NOT NULL OR ($2).prerelease IS NOT NULL OR ($2).build IS NOT NULL THEN NULL
+        WHEN ($1).major = 0 AND ($1).minor = 0 AND ($1).bug = 0 THEN 'zero_to_something'::analysis.update_type
+        WHEN ($1).major = ($2).major AND ($1).minor = ($2).minor THEN 'bug'::analysis.update_type
+        WHEN ($1).major = ($2).major THEN 'minor'::analysis.update_type
+        ELSE 'major'::analysis.update_type
+    END $$ LANGUAGE SQL IMMUTABLE;
+
 CREATE UNLOGGED TABLE analysis.all_updates AS WITH intra_group_updates AS (
-    SELECT from_v.group_base_semver AS group_base_semver,
-        from_v.package_id AS package_id,
+    SELECT from_v.package_id AS package_id,
+        from_v.group_base_semver AS from_group_base_semver,
+        to_v.group_base_semver AS to_group_base_semver,
         from_v.id AS from_id,
         to_v.id AS to_id,
         from_v.semver AS from_semver,
         to_v.semver AS to_semver,
         from_v.created AS from_created,
-        to_v.created AS to_created
+        to_v.created AS to_created,
+        analysis.determine_update_type(from_v.semver, to_v.semver) AS ty
     FROM analysis.valid_non_betas_with_ordering from_v
         INNER JOIN analysis.valid_non_betas_with_ordering to_v ON from_v.package_id = to_v.package_id
         AND from_v.group_base_semver = to_v.group_base_semver
@@ -27,14 +43,16 @@ selected_inter_group_predecessors AS (
         -- but its necessary to put in the GROUP BY to include in the SELECT
 ),
 inter_group_updates AS (
-    SELECT from_v.group_base_semver AS group_base_semver,
-        from_v.package_id AS package_id,
+    SELECT from_v.package_id AS package_id,
+        from_v.group_base_semver AS from_group_base_semver,
+        to_v.group_base_semver AS to_group_base_semver,
         from_v.id AS from_id,
         to_v.id AS to_id,
         from_v.semver AS from_semver,
         to_v.semver AS to_semver,
         from_v.created AS from_created,
-        to_v.created AS to_created
+        to_v.created AS to_created,
+        analysis.determine_update_type(from_v.semver, to_v.semver) AS ty
     FROM selected_inter_group_predecessors preds
         INNER JOIN analysis.valid_non_betas_with_ordering from_v ON from_v.package_id = preds.package_id
         AND from_v.chron_order_global = preds.greatest_lower_chron_order_global
