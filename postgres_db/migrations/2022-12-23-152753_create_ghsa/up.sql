477b1e68addb4a491c5455dd648ed49defd34bcc
Donald Pinckney <donald_pinckney@icloud.com>
2022-12-30 19:45:05 -0500

    schema tweaks

--- postgres_db/migrations/2022-12-23-152753_create_ghsa/up.sql
+++ postgres_db/migrations/2022-12-23-152753_create_ghsa/up.sql
@@ -1,14 +1,40 @@
 CREATE TABLE ghsa (
-  id TEXT PRIMARY KEY NOT NULL, -- GHSA id
-  severity TEXT NOT NULL, -- Severity of the vulnerability
-  description TEXT NOT NULL, -- Description of the vulnerability
-  summary TEXT NOT NULL, -- Summary of the vulnerability
-  withdrawn_at TIMESTAMP WITH TIME ZONE, -- Date when the vulnerability was withdrawn (if any)
-  published_at TIMESTAMP WITH TIME ZONE NOT NULL, -- Date when the vulnerability was published
-  updated_at TIMESTAMP WITH TIME ZONE NOT NULL, -- Date when the vulnerability was last updated
-  refs TEXT[] NOT NULL, -- References (URLS) to the vulnerability
-  cvss_score real, -- CVSS score of the vulnerability
-  cvss_vector TEXT, -- CVSS vector of the vulnerability
-  packages TEXT[] NOT NULL, -- Packages affected by the vulnerability
-  vulns JSONB NOT NULL -- Vulnerabilities (JSONB)
+  id TEXT PRIMARY KEY NOT NULL,
+  -- GHSA id
+  severity TEXT NOT NULL,
+  -- Severity of the vulnerability
+  description TEXT NOT NULL,
+  -- Description of the vulnerability
+  summary TEXT NOT NULL,
+  -- Summary of the vulnerability
+  withdrawn_at TIMESTAMP WITH TIME ZONE,
+  -- Date when the vulnerability was withdrawn (if any)
+  published_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  -- Date when the vulnerability was published
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  -- Date when the vulnerability was last updated
+  refs TEXT [] NOT NULL,
+  -- References (URLS) to the vulnerability
+  cvss_score real,
+  -- CVSS score of the vulnerability
+  cvss_vector TEXT -- CVSS vector of the vulnerability
+);
+
+
+CREATE TABLE vulnerabilities (
+  ghsa_id TEXT NOT NULL REFERENCES ghsa (id),
+  package_name TEXT NOT NULL,
+  vulnerable_version_lower_bound semver,
+  vulnerable_version_lower_bound_inclusive BOOLEAN NOT NULL,
+  vulnerable_version_upper_bound semver,
+  vulnerable_version_upper_bound_inclusive BOOLEAN NOT NULL,
+  first_patched_version semver,
+  PRIMARY KEY (
+    ghsa_id,
+    package_name,
+    vulnerable_version_lower_bound,
+    vulnerable_version_lower_bound_inclusive,
+    vulnerable_version_upper_bound,
+    vulnerable_version_upper_bound_inclusive
+  )
 );
