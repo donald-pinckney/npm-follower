b62fee0132efa3b27f40b90060270f80bd99b2ff
Donald Pinckney <donald_pinckney@icloud.com>
2023-01-03 14:04:47 -0500

    x

--- historic_npm_registry/src/main.rs
+++ historic_npm_registry/src/main.rs
@@ -1,12 +1,37 @@
+use chrono::DateTime;
+use chrono::Utc;
+use serde_json::Value;
 use warp::Filter;
+
+async fn lookup_package(client: reqwest::Client, t: DateTime<Utc>, name: String) -> Value {
+    println!("{}, {}", t, name);
+    Value::Null
+    // todo!()
+}
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let req_client = reqwest::Client::new();
+    let req_client2 = req_client.clone();
 
-    warp::serve(hello)
+    let non_scoped = warp::path::param::<DateTime<Utc>>()
+        .and(warp::path::param::<String>())
+        .and(warp::any().map(move || req_client.clone()))
+        .then(|t, name, req_client_inner| async move {
+            warp::reply::json(&lookup_package(req_client_inner, t, name).await)
+        });
+
+    let scoped = warp::path::param::<DateTime<Utc>>()
+        .and(warp::path::param::<String>())
+        .and(warp::path::param::<String>())
+        .and(warp::any().map(move || req_client2.clone()))
+        .then(|t, scope, name, req_client_inner| async move {
+            warp::reply::json(
+                &lookup_package(req_client_inner, t, format!("{}/{}", scope, name)).await,
+            )
+        });
+
+    warp::serve(non_scoped.or(scoped))
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
