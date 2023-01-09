79137778a817c2144358c09782844a9f488e4b40
elleven11 <federico.cassano@federico.codes>
2023-01-08 21:22:09 -0500

    2. and 3.

--- diff_queries/src/main.rs
+++ diff_queries/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use diesel::QueryableByName;
 use postgres_db::{
     connection::{DbConnection, QueryRunner},
@@ -6,7 +8,7 @@ use postgres_db::{
 use serde::{Deserialize, Serialize};
 
 fn print_usage_exit(argv0: &str) -> ! {
-    eprintln!("Usage: {} [num_files] chunk_size", argv0);
+    eprintln!("Usage: {} [num_files|num_lines|ext_count] chunk_size", argv0);
     std::process::exit(1);
 }
 
@@ -21,6 +23,8 @@ fn main() {
     let mut conn: DbConnection = DbConnection::connect();
     let write_func = match args[1].as_str() {
         "num_files" => num_files,
+        "num_lines" => num_lines,
+        "ext_count" => ext_count,
         _ => {
             print_usage_exit(args[0].as_str());
         }
@@ -76,7 +80,7 @@ fn num_files(
     let mut num_files: Vec<NumFiles> = vec![];
     for diff in diffs {
         let mut num_files_added = 0;
-        let mut num_files_removed = 0;
+        let mut num_files_deleted = 0;
         let mut num_files_modified = 0;
         match &diff.job_result {
             DiffAnalysisJobResult::Diff(map) => {
@@ -88,7 +92,7 @@ fn num_files(
                     {
                         num_files_modified += 1;
                     } else if diff.total_new.is_none() {
-                        num_files_removed += 1;
+                        num_files_deleted += 1;
                     } else if diff.total_old.is_none() {
                         num_files_added += 1;
                     }
@@ -101,7 +105,7 @@ fn num_files(
             to_id: diff.to_id,
             num_files_added,
             num_files_modified,
-            num_files_deleted: num_files_removed,
+            num_files_deleted,
         });
     }
 
@@ -126,6 +130,133 @@ fn num_files(
     }
     query.push_str(" ON CONFLICT (from_id, to_id) DO NOTHING");
     println!("Inserting {} rows", num_files.len());
+    let diesel_query = diesel::sql_query(query);
+    conn.execute(diesel_query)?;
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, QueryableByName, Debug, Clone)]
+struct NumLines {
+    #[sql_type = "diesel::sql_types::BigInt"]
+    from_id: i64,
+    #[sql_type = "diesel::sql_types::BigInt"]
+    to_id: i64,
+    #[sql_type = "diesel::sql_types::BigInt"]
+    num_lines_added: i64,
+    #[sql_type = "diesel::sql_types::BigInt"]
+    num_lines_deleted: i64,
+}
+
+fn num_lines(
+    conn: &mut DbConnection,
+    diffs: Vec<DiffAnalysis>,
+) -> Result<(), diesel::result::Error> {
+    let mut num_lines: Vec<NumLines> = vec![];
+    for diff in diffs {
+        let mut num_lines_added = 0;
+        let mut num_lines_deleted = 0;
+        match &diff.job_result {
+            DiffAnalysisJobResult::Diff(map) => {
+                for diff in map.values() {
+                    num_lines_added += diff.added as i64;
+                    num_lines_deleted += diff.removed as i64;
+                }
+            }
+            _ => continue,
+        }
+        num_lines.push(NumLines {
+            from_id: diff.from_id,
+            to_id: diff.to_id,
+            num_lines_added,
+            num_lines_deleted,
+        });
+    }
+
+    if num_lines.is_empty() {
+        return Ok(());
+    }
+
+    // insert into analysis.diff_num_lines
+    let query = r#"
+        INSERT INTO analysis.diff_num_lines (from_id, to_id, num_lines_added, num_lines_deleted)
+        VALUES
+        "#;
+    let mut query = query.to_string();
+    for (i, nl) in num_lines.iter().enumerate() {
+        if i > 0 {
+            query.push_str(", ");
+        }
+        query.push_str(&format!(
+            "({}, {}, {}, {})",
+            nl.from_id, nl.to_id, nl.num_lines_added, nl.num_lines_deleted,
+        ));
+    }
+    query.push_str(" ON CONFLICT (from_id, to_id) DO NOTHING");
+    println!("Inserting {} rows", num_lines.len());
+    let diesel_query = diesel::sql_query(query);
+    conn.execute(diesel_query)?;
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, QueryableByName, Debug, Clone)]
+struct ExtCount {
+    #[sql_type = "diesel::sql_types::Text"]
+    ext: String,
+    #[sql_type = "diesel::sql_types::BigInt"]
+    count: i64,
+}
+
+fn get_extension(path: &str) -> Option<&str> {
+    let split: Vec<&str> = path.split('.').collect();
+    if split.len() > 1 {
+        Some(split[split.len() - 1])
+    } else {
+        None
+    }
+}
+
+fn ext_count(
+    conn: &mut DbConnection,
+    diffs: Vec<DiffAnalysis>,
+) -> Result<(), diesel::result::Error> {
+    let mut ext_counts: HashMap<String, i64> = HashMap::new();
+    for diff in diffs {
+        match &diff.job_result {
+            DiffAnalysisJobResult::Diff(map) => {
+                for diff in map.keys() {
+                    let ext = match get_extension(diff) {
+                        Some(e) => e,
+                        None => continue,
+                    };
+                    let count = ext_counts.entry(ext.to_string()).or_insert(0);
+                    *count += 1;
+                }
+            }
+            _ => continue,
+        }
+    }
+
+    if ext_counts.is_empty() {
+        return Ok(());
+    }
+
+    // insert into analysis.diff_ext_count
+    let query = r#"
+        INSERT INTO analysis.diff_ext_count (ext, count)
+        VALUES
+        "#;
+    let mut query = query.to_string();
+    for (i, (ext, count)) in ext_counts.iter().enumerate() {
+        if i > 0 {
+            query.push_str(", ");
+        }
+        query.push_str(&format!("('{}', {})", ext, count));
+    }
+    // on conflict, update count += excluded.count
+    query.push_str(
+        " ON CONFLICT (ext) DO UPDATE SET count = analysis.diff_ext_count.count + excluded.count",
+    );
+    println!("Inserting {} rows", ext_counts.len());
     let diesel_query = diesel::sql_query(query);
     conn.execute(diesel_query)?;
     Ok(())
