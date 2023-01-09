ef7409cedf567af496514a138259ac302f274896
Donald Pinckney <donald_pinckney@icloud.com>
2023-01-09 16:40:34 -0500

    finalize subsamling algo

--- analysis/scripts/subsampled_updates.sql
+++ analysis/scripts/subsampled_updates.sql
@@ -3,6 +3,16 @@ CREATE TABLE analysis.subsampled_updates AS WITH filtered_updates as (
   from analysis.all_updates
   where to_created < TIMESTAMP WITH TIME ZONE '2021-01-01 00:00:00+00'
     and ty <> 'zero_to_something'
+    and ROW(from_id, to_id) NOT IN (
+      SELECT from_id,
+        to_id
+      FROM analysis.vuln_patch_updates
+    )
+    and ROW(from_id, to_id) NOT IN (
+      SELECT from_id,
+        to_id
+      FROM analysis.vuln_intro_updates
+    )
 ),
 ranked_updates as (
   SELECT *,
@@ -21,13 +31,26 @@ SELECT package_id,
   from_created,
   to_created,
   ty,
-  NULL as patched_ghsa
+  NULL as patched_ghsa,
+  NULL as introduced_ghsa
 FROM ranked_updates
 WHERE date_rank = 1
-  and ROW(from_id, to_id) NOT IN (
+UNION ALL
+SELECT package_id,
+  from_id,
+  to_id,
+  from_semver,
+  to_semver,
+  from_created,
+  to_created,
+  ty,
+  patched_ghsa,
+  NULL as introduced_ghsa
+FROM analysis.vuln_patch_updates
+where ROW(from_id, to_id) NOT IN (
     SELECT from_id,
       to_id
-    FROM analysis.vuln_patch_updates
+    FROM analysis.vuln_intro_updates
   )
 UNION ALL
 SELECT package_id,
@@ -38,8 +61,29 @@ SELECT package_id,
   from_created,
   to_created,
   ty,
-  patched_ghsa
-FROM analysis.vuln_patch_updates
+  NULL as patched_ghsa,
+  introduced_ghsa
+FROM analysis.vuln_intro_updates
+where ROW(from_id, to_id) NOT IN (
+    SELECT from_id,
+      to_id
+    FROM analysis.vuln_patch_updates
+  )
+UNION ALL
+SELECT i.package_id,
+  i.from_id,
+  i.to_id,
+  i.from_semver,
+  i.to_semver,
+  i.from_created,
+  i.to_created,
+  i.ty,
+  p.patched_ghsa,
+  i.introduced_ghsa
+FROM analysis.vuln_intro_updates i
+  inner join analysis.vuln_patch_updates p on i.from_id = p.from_id
+  and i.to_id = p.to_id;
+
 ALTER TABLE analysis.subsampled_updates
 ADD PRIMARY KEY (from_id, to_id);
 
