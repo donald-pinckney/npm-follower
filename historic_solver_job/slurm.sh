cb470dc410a304988cda82cab0d683b4b4010e5f
Donald Pinckney <donald_pinckney@icloud.com>
2023-01-10 21:27:42 -0500

    x

--- historic_solver_job/slurm.sh
+++ historic_solver_job/slurm.sh
@@ -6,7 +6,6 @@
 #SBATCH --time=24:00:00
 #SBATCH --job-name=historic_solver_job
 #SBATCH --partition=short
-#SBATCH --constraint=haswell|broadwell|skylake_avx512|zen2|zen|cascadelake
 
 ssh -L 5431:localhost:5432  pinckney@pinckney2.vpc.ripley.cloud -N -f
 ./launch.sh
