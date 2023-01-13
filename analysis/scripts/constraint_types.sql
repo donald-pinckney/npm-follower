a2d974d9bc7f940cf0a406ab86ac279b0d01897d
Donald Pinckney <donald_pinckney@icloud.com>
2023-01-13 20:09:51 +0000

    x

--- analysis/scripts/constraint_types.sql
+++ analysis/scripts/constraint_types.sql
@@ -6,42 +6,40 @@
 -- we return NULL.
 -- also, we have `A || B`, `A, B`, `A - B`, and others.
 CREATE OR REPLACE FUNCTION analysis.check_constraint_type(constraint_string text) RETURNS text AS $$
-DECLARE
-    constraint_type text;
-BEGIN
-    IF constraint_string LIKE '%|%' THEN
-        constraint_type := '||';
-    ELSIF constraint_string LIKE '%,%' THEN
-        constraint_type := ',';
-    ELSIF constraint_string LIKE '% - %' THEN
-        constraint_type := '-';
-    ELSIF constraint_string LIKE '"^%' OR constraint_string LIKE '" ^%' THEN
-        constraint_type := 'minor';
-    ELSIF constraint_string LIKE '"~%' OR constraint_string LIKE '" ~%' THEN
-        constraint_type := 'patch';
-    ELSIF constraint_string LIKE '"<=%' OR constraint_string LIKE '" <=%' THEN
-        constraint_type := '<=';
-    ELSIF constraint_string LIKE '">=%' OR constraint_string LIKE '" >=%' THEN
-        constraint_type := '>=';
-    ELSIF constraint_string LIKE '"<%' OR constraint_string LIKE '" <%' THEN
-        constraint_type := '<';
-    ELSIF constraint_string LIKE '">%' OR constraint_string LIKE '" >%' THEN
-        constraint_type := '>';
-    ELSIF constraint_string LIKE '"=%' OR constraint_string LIKE '" =%' THEN
-        constraint_type := '=';
-    ELSIF constraint_string ~ '^"\ *v?[0-9]+\.[0-9]+\.[x||X||\*]"$' THEN
-        constraint_type := 'patch';
-    ELSIF constraint_string ~ '^"\ *v?[0-9]+\.[x||X||\*](\.[x||X||\*])?"$' THEN
-        constraint_type := 'minor';
-    ELSIF constraint_string ~ '^"\ *v?[x||X||\*]\.[x||X||\*]\.[x||X||\*]"$' THEN
-        constraint_type := 'major'; -- same thing as '*'
-    ELSIF constraint_string ~ '^"\ *v?[0-9]+\.[0-9]+"$' THEN
-        constraint_type := 'patch';
-    ELSIF constraint_string ~ '^"\ *v?[0-9]+"$' THEN
-        constraint_type := 'minor';
-    ELSIF constraint_string ~ '^"\ *v?[0-9]+\.[0-9]+\.[0-9]+-?.*"$' THEN
-        constraint_type := '='; -- essentially a `=`
-    ELSIF constraint_string LIKE '"*"' 
+    SELECT CASE 
+    WHEN constraint_string LIKE '%|%' THEN
+        '||'
+    WHEN constraint_string LIKE '%,%' THEN
+        ','
+    WHEN constraint_string LIKE '% - %' THEN
+        '-'
+    WHEN constraint_string LIKE '"^%' OR constraint_string LIKE '" ^%' THEN
+        'minor'
+    WHEN constraint_string LIKE '"~%' OR constraint_string LIKE '" ~%' THEN
+        'patch'
+    WHEN constraint_string LIKE '"<=%' OR constraint_string LIKE '" <=%' THEN
+        '<='
+    WHEN constraint_string LIKE '">=%' OR constraint_string LIKE '" >=%' THEN
+        '>='
+    WHEN constraint_string LIKE '"<%' OR constraint_string LIKE '" <%' THEN
+        '<'
+    WHEN constraint_string LIKE '">%' OR constraint_string LIKE '" >%' THEN
+        '>'
+    WHEN constraint_string LIKE '"=%' OR constraint_string LIKE '" =%' THEN
+        '='
+    WHEN constraint_string ~ '^"\ *v?[0-9]+\.[0-9]+\.[x||X||\*]"$' THEN
+        'patch'
+    WHEN constraint_string ~ '^"\ *v?[0-9]+\.[x||X||\*](\.[x||X||\*])?"$' THEN
+        'minor'
+    WHEN constraint_string ~ '^"\ *v?[x||X||\*]\.[x||X||\*]\.[x||X||\*]"$' THEN
+        'major' -- same thing as '*'
+    WHEN constraint_string ~ '^"\ *v?[0-9]+\.[0-9]+"$' THEN
+        'patch'
+    WHEN constraint_string ~ '^"\ *v?[0-9]+"$' THEN
+        'minor'
+    WHEN constraint_string ~ '^"\ *v?[0-9]+\.[0-9]+\.[0-9]+-?.*"$' THEN
+        '=' -- essentially a `=`
+    WHEN constraint_string LIKE '"*"' 
       OR constraint_string LIKE '" *"' 
       OR constraint_string LIKE '""' 
       OR constraint_string LIKE '"x"' 
@@ -50,20 +48,20 @@ BEGIN
       OR constraint_string LIKE '" x.*"'
       OR constraint_string LIKE '"x.x"'
       OR constraint_string LIKE '" x.x"' THEN 
-        constraint_type := 'major';
+        'major'
     ELSE
-        constraint_type := 'invalid';
-    END IF;
-    RETURN constraint_type;
-END;
-$$ LANGUAGE plpgsql;
+        'invalid'
+END
+$$ LANGUAGE SQL IMMUTABLE;
 
 
 CREATE TABLE analysis.constraint_types AS
 SELECT 
     id as dependency_id, 
-    CASE (spec).dep_type WHEN 'range' THEN analysis.check_constraint_type(raw_spec)
-                         ELSE NULL
+    CASE WHEN (spec).dep_type = 'range' AND 
+              jsonb_typeof(raw_spec) = 'string' 
+              THEN analysis.check_constraint_type(raw_spec #>> '{}')
+         ELSE NULL
     END as constraint_type
 FROM dependencies;
 
