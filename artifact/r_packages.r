8c072e2a1e9f8b0f0bc5e08c7c59ea0cae5c16c1
Donald Pinckney <donald_pinckney@icloud.com>
2023-01-19 00:40:06 +0000

    x

--- artifact/r_packages.r
+++ artifact/r_packages.r
@@ -1,2 +1,2 @@
-install.packages(c("DBI", "tidyverse", "caret", "scales", "ggalluvial", "remotes"))
+install.packages(c("DBI", "tidyverse", "caret", "scales", "ggalluvial", "remotes", "RPostgres"))
 remotes::install_github("davidsjoberg/ggsankey")
