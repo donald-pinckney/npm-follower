a66e41afe2de52a71ea716e5014949b2d59661f6
elleven11 <federico.cassano@federico.codes>
2023-02-02 13:45:24 -0500

    sleep if no downloads

--- downloader/continuous_script.sh
+++ downloader/continuous_script.sh
@@ -32,8 +32,19 @@ cd $SCRIPT_PATH/..
 
 # loop forever
 while true; do 
-  # exit if download_queuer or downloader exit with error
+  # exit if download_queuer or downloader exit with error.
   cargo run --release --bin download_queuer || exit 1
-  cargo run --release --bin $RUST_BIN -- $WORKERS || exit 1
+
+  # if downloader returns "0 tasks to download" then sleep for 30 seconds
+  OUTPUT=$(cargo run --release --bin $RUST_BIN -- $WORKERS || exit 1)
+  # check $? for exit code
+  if [ $? -eq 1 ]; then
+    exit 1
+  fi
+  # check if first 1000 lines of output contains "0 tasks to download"
+  if echo "$OUTPUT" | head -n 1000 | grep -q "0 tasks to download"; then
+    echo "Sleeping for 30 seconds"
+    sleep 30
+  fi
 done
 
