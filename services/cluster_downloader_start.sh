8fd238bf3fe18f9efcb03ac6740bc37d1ed427d1
elleven11 <federico.cassano@federico.codes>
2023-04-13 10:10:34 -0400

    job service

--- services/cluster_downloader_start.sh
+++ services/cluster_downloader_start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-cd /home/federico/npm-follower/downloader
-./continuous_script.sh cluster 2
+
+# change these to be your own users
+FOLLOWER_USER="federico"
+
+export PATH="$PATH:/home/$FOLLOWER_USER/.cargo/bin/"
+source "/home/$FOLLOWER_USER/.cargo/env"
+cd /home/$FOLLOWER_USER/npm-follower/downloader
+sudo -u $FOLLOWER_USER ./continuous_script.sh cluster 2
