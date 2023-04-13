4cc5dab2d4f489e3f960d3e374d56e7758d7a0b2
elleven11 <federico.cassano@federico.codes>
2023-04-13 10:14:41 -0400

    fix cargo path

--- services/cluster_downloader_start.sh
+++ services/cluster_downloader_start.sh
@@ -3,7 +3,5 @@
 # change these to be your own users
 FOLLOWER_USER="federico"
 
-export PATH="$PATH:/home/$FOLLOWER_USER/.cargo/bin/"
-source "/home/$FOLLOWER_USER/.cargo/env"
 cd /home/$FOLLOWER_USER/npm-follower/downloader
 sudo -u $FOLLOWER_USER ./continuous_script.sh cluster 2
--- services/job_scheduler_start.sh
+++ services/job_scheduler_start.sh
@@ -2,12 +2,11 @@
 
 # change these to be your own users
 FOLLOWER_USER="federico"
+CARGO_PATH="$PATH:/home/$FOLLOWER_USER/.cargo/bin/cargo"
 
 # change to increase/decrease number of workers
 NUM_XFER_WORKERS=2
 NUM_COMP_WORKERS=0
 
-export PATH="$PATH:/home/$FOLLOWER_USER/.cargo/bin/"
-source "/home/$FOLLOWER_USER/.cargo/env"
 cd /home/$FOLLOWER_USER/npm-follower/blob_idx_server/
-sudo -u $FOLLOWER_USER cargo run --release -- $NUM_COMP_WORKERS $NUM_XFER_WORKERS
+sudo -u $FOLLOWER_USER $CARGO_PATH run --release -- $NUM_COMP_WORKERS $NUM_XFER_WORKERS
