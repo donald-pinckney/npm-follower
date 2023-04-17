e2fcf33254876d3029dd47436eac7f63af29eeea
Donald Pinckney <donald_pinckney@icloud.com>
2023-04-17 14:12:04 -0400

    script cleanup

--- database_exporting/avc_add_all_blob_files.py
+++ database_exporting/avc_add_all_blob_files.py
@@ -4,20 +4,16 @@ import os
 
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python avc_add_all_blob_files.py <path_to_blob_dir>")
-        return
+    a = avc.Avc(data_dir=None, initialize=False)
+    blobs_dir = a.data_toplevel
 
-    blobs_dir = sys.argv[1]
-    a = avc.Avc(data_dir=blobs_dir, initialize=False)
+    print(blobs_dir)
 
     # Loop over files in blobs_dir with .bin extension:
     for file in os.listdir(blobs_dir):
         if file.endswith(".bin"):
             a.add(file, None)
             # avc.add(file)
-
-    print(blobs_dir)
 
 
 if __name__ == "__main__":
