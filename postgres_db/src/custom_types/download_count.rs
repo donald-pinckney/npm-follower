c56e7f384c7ca69f85d201db952f310fdff62e31
Donald Pinckney <donald_pinckney@icloud.com>
2023-08-04 15:42:39 +0800

    fix download_count.rs

--- postgres_db/src/custom_types/download_count.rs
+++ postgres_db/src/custom_types/download_count.rs
@@ -1,23 +1,22 @@
 use diesel::{
-    pg::Pg,
-    serialize::WriteTuple,
-    sql_types::{BigInt, Date},
-    types::{FromSql, Record, ToSql},
+    pg::{Pg, PgValue},
+    serialize::{WriteTuple, ToSql, Output, self},
+    sql_types::{BigInt, Date, Record}, deserialize::{FromSql, self},
 };
 
 use super::{sql_types::DownloadCountSql, DownloadCount};
 
 impl ToSql<DownloadCountSql, Pg> for DownloadCount {
-    fn to_sql<W: std::io::Write>(
+    fn to_sql(
         &self,
-        out: &mut diesel::serialize::Output<W, Pg>,
-    ) -> diesel::serialize::Result {
+        out: &mut Output<Pg>,
+    ) -> serialize::Result {
         WriteTuple::<(Date, BigInt)>::write_tuple(&(self.date, self.count), out)
     }
 }
 
 impl FromSql<DownloadCountSql, Pg> for DownloadCount {
-    fn from_sql(bytes: Option<&[u8]>) -> diesel::deserialize::Result<Self> {
+    fn from_sql(bytes: PgValue) -> deserialize::Result<Self> {
         let (date, count) = FromSql::<Record<(Date, BigInt)>, Pg>::from_sql(bytes)?;
         Ok(DownloadCount { date, count })
     }
